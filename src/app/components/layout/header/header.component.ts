4cc6809b57183120b303265c2b8a11ef2cefccde
AMI2080 <AMI_2080@Yahoo.com>
2024-03-09 02:29:42 +0200

    language method

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -4,21 +4,51 @@ import { faUser } from '@fortawesome/free-regular-svg-icons';
 import {
   faAddressCard,
   faArrowRightFromBracket,
+  faGlobe,
 } from '@fortawesome/free-solid-svg-icons';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+
+interface Language {
+  key: string;
+  name: string;
+  label: string;
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  public languages: Language[] = [
+    {
+      key: 'en',
+      name: 'english',
+      label: 'translate_english',
+    },
+    {
+      key: 'ar',
+      name: 'arabic',
+      label: 'translate_arabic',
+    },
+  ];
+
   public constructor(
     private readonly authService: AuthService,
+    private readonly translateService: TranslateService,
     private readonly router: Router,
     readonly fa: FaIconLibrary
   ) {
-    fa.addIcons(faUser, faAddressCard, faArrowRightFromBracket);
+    fa.addIcons(faUser, faAddressCard, faArrowRightFromBracket, faGlobe);
+  }
+
+  public changeLanguage(lang: string): void {
+    this.translateService.use(lang);
+  }
+
+  public isCurrentLang(lang: Language): boolean {
+    return this.translateService.currentLang === lang.key;
   }
 
   public logout(): void {
